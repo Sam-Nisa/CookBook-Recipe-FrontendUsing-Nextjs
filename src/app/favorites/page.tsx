001cf9947c91a4585bb6c2622d9ce65461c117ed
Sam Nisa <samnisa2104@gmail.com>
2026-06-26 17:02:08 +0700

    finish all feature

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -47,7 +47,7 @@ export default function FavoritesPage() {
         ) : (
           /* Favorites Grid */
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {favorites.map((recipe) => (
+            {favorites.map((recipe: any) => (
               <RecipeCard key={recipe.id} recipe={recipe} />
             ))}
           </div>
