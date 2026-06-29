d471d0b1c6f8ca1c545cd7f3dfbc42f367a422b6
Sam Nisa <samnisa2104@gmail.com>
2026-06-29 11:00:30 +0700

    fix bugs

--- src/data/categories.js
+++ src/data/categories.js
@@ -3,25 +3,25 @@ const categories = [
     id: 1,
     name: "Breakfast",
     image: "/images/categories/breakfast.png",
-    recipeCount: 48
+    recipeCount: 4
   },
   {
     id: 2,
     name: "Lunch",
     image: "/images/categories/lunch.png",
-    recipeCount: 124
+    recipeCount: 4
   },
   {
     id: 3,
     name: "Dinner",
     image: "/images/categories/dinner.png",
-    recipeCount: 210
+    recipeCount: 4
   },
   {
     id: 4,
     name: "Dessert",
     image: "/images/categories/dessert.png",
-    recipeCount: 86
+    recipeCount: 3
   }
 ];
 
